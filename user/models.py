4dfeccaf77d602504848342d1238d710c7718163
Akatosh, the God of Time and Order <borjan_blizz@hotmail.com>
2018-06-24 15:30:53 +0200

    Change datetime now

--- user/models.py
+++ user/models.py
@@ -1,9 +1,10 @@
 from django.db import models
 from django.contrib.auth.models import User
+import datetime
 
 
 class ProfileModel(models.Model):
 
     user = models.ForeignKey(User, on_delete=models.CASCADE, related_name='profiles')
     image_path = models.CharField(max_length=255)
-    date_registered = models.DateTimeField(auto_now_add=True)
+    date_registered = models.DateTimeField(default=datetime.datetime.now())
